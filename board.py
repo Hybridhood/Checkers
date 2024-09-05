217eb52191f5ae748683b747c2df469017b643cd
Hybridhood <67559008+Hybridhood@users.noreply.github.com>
2024-09-05 19:57:32 -0400

    Add files via upload

--- board.py
+++ board.py
@@ -31,18 +31,22 @@ class Board:
         for x in range(len(self.brd)):
             for y in range(len(self.brd)):
                 if self.brd[x][y] == "X":
-                    pygame.draw.circle(self.screen, "white", (y * self.x_size + self.x_size / 2, x * self.y_size + self.y_size / 2), 20)
+                    pygame.draw.circle(self.screen, "white", (y * self.x_size + self.x_size / 2, x * self.y_size + self.y_size / 2), 50)
                 elif self.brd[x][y] == "O":
-                    pygame.draw.circle(self.screen, "black", (y * self.x_size + self.x_size / 2, x * self.y_size + self.y_size / 2), 20)
+                    pygame.draw.circle(self.screen, "black", (y * self.x_size + self.x_size / 2, x * self.y_size + self.y_size / 2), 50)
                 elif self.brd[x][y] == ".":
-                    pygame.draw.circle(self.screen, "blue", (y * self.x_size + self.x_size / 2, x * self.y_size + self.y_size / 2), 4)
+                    pygame.draw.circle(self.screen, "blue", (y * self.x_size + self.x_size / 2, x * self.y_size + self.y_size / 2), 10)
+                elif self.brd[x][y] == "L":
+                    pygame.draw.circle(self.screen, "green", (y * self.x_size + self.x_size / 2, x * self.y_size + self.y_size / 2), 10)
+                elif self.brd[x][y] == "R":
+                    pygame.draw.circle(self.screen, "green", (y * self.x_size + self.x_size / 2, x * self.y_size + self.y_size / 2), 10)
 
 
     def select_piece(self):
         self.mousepress = not self.mousepress
         x, y = pygame.mouse.get_pos()
-        col = x // 50  
-        row = y // 50
+        col = x // 125  
+        row = y // 125
         piece_on = row, col
         if self.brd[piece_on[0]][piece_on[1]] == "X":
             if piece_on == self.selected:
@@ -55,14 +59,12 @@ class Board:
                     if self.selected[0] + 1 < 8:
                         if self.selected[1] - 1 >= 0 and self.brd[self.selected[0]+1][self.selected[1]-1] == 0:
                             self.brd[self.selected[0]+1][self.selected[1]-1] = "."
-                        if self.selected[1] - 1 >= 0 and self.brd[self.selected[0]+1][self.selected[1]-1] == "O" and self.brd[self.selected[0]+2][self.selected[1]-2] == 0:
-                            self.brd[self.selected[0]+1][self.selected[1]-1] = 0
-                            self.brd[self.selected[0]+2][self.selected[1]-2] = "."
                         if self.selected[1] + 1 < 8 and self.brd[self.selected[0]+1][self.selected[1]+1] == 0:
                             self.brd[self.selected[0]+1][self.selected[1]+1] = "."
-                        if self.selected[1] - 1 >= 0 and self.brd[self.selected[0]+1][self.selected[1]+1] == "O" and self.brd[self.selected[0]+2][self.selected[1]-2] == 0:
-                            self.brd[self.selected[0]+1][self.selected[1]+1] == "O"
-                            self.brd[self.selected[0]+2][self.selected[1]+2] = "."
+                        if self.selected[1] + 1 < 8 and self.brd[self.selected[0]+1][self.selected[1]+1] == "O" and self.brd[self.selected[0]+2][self.selected[1]+2] == 0:
+                            self.brd[self.selected[0]+2][self.selected[1]+2] = "R"
+                        if self.selected[1] + 1 < 8 and self.brd[self.selected[0]+1][self.selected[1]-1] == "O" and self.brd[self.selected[0]+2][self.selected[1]-2] == 0:
+                            self.brd[self.selected[0]+2][self.selected[1]-2] = "L"
             except IndexError:
                 print("Out of range")
 
@@ -79,6 +81,10 @@ class Board:
                             self.brd[self.selected[0]-1][self.selected[1]-1] = "."
                         if self.selected[1] + 1 < 8 and self.brd[self.selected[0]-1][self.selected[1]+1] == 0:
                             self.brd[self.selected[0]-1][self.selected[1]+1] = "."
+                        if self.selected[1] + 1 < 8 and self.brd[self.selected[0]-1][self.selected[1]-1] == "X" and self.brd[self.selected[0]-2][self.selected[1]-2] == 0:
+                            self.brd[self.selected[0]-2][self.selected[1]-2] = "R"
+                        if self.selected[1] + 1 < 8 and self.brd[self.selected[0]-1][self.selected[1]+1] == "X" and self.brd[self.selected[0]-2][self.selected[1]+2] == 0:
+                            self.brd[self.selected[0]-2][self.selected[1]+2] = "L"
             except IndexError:
                 print("Out of range")
         self.check_for_move(piece_on)
@@ -86,23 +92,34 @@ class Board:
     def clear_possible_moves(self):
         for i in range(len(self.brd)):
             for z in range(len(self.brd)):
-                if self.brd[i][z] == ".":
+                if self.brd[i][z] == "." or self.brd[i][z] == "L" or self.brd[i][z] == "R":
                     self.brd[i][z] = 0
         self.selected = None
 
     def check_for_move(self, piece_on):
         if self.selected != piece_on:
+           #  or self.brd[piece_on[0]][piece_on[1]] == "L" or self.brd[piece_on[0]][piece_on[1]] == "R"
             if self.brd[piece_on[0]][piece_on[1]] == 0:
                 self.clear_possible_moves()
+                return
+        if self.selected is None:
+            if self.brd[piece_on[0]][piece_on[1]] == "L" or self.brd[piece_on[0]][piece_on[1]] == "R":
+                print("uhhhh")
                 return
         if self.brd[self.selected[0]][self.selected[1]] == "X":
             if self.brd[piece_on[0]][piece_on[1]] == ".":
                 self.move_piece_to(piece_on, "X")
-                
+            if self.brd[piece_on[0]][piece_on[1]] == "L":
+                self.capture_piece(piece_on)
+            if self.brd[piece_on[0]][piece_on[1]] == "R":
+                self.capture_piece(piece_on)
         elif self.brd[self.selected[0]][self.selected[1]] == "O":
             if self.brd[piece_on[0]][piece_on[1]] == ".":
                 self.move_piece_to(piece_on, "O")
-                
+            if self.brd[piece_on[0]][piece_on[1]] == "L":
+                self.capture_piece(piece_on)
+            if self.brd[piece_on[0]][piece_on[1]] == "R":
+                self.capture_piece(piece_on)
         elif self.brd[piece_on[0]][piece_on[1]] == 0:
             self.clear_possible_moves()
             
@@ -111,6 +128,34 @@ class Board:
         self.brd[self.selected[0]][self.selected[1]] = 0
         self.brd[piece_on[0]][piece_on[1]] = piece_type
         self.clear_possible_moves()
+    def capture_piece(self,piece_on):
+        print(piece_on)
+        if self.brd[piece_on[0]][piece_on[1]] == "L":
+            if self.brd[self.selected[0]][self.selected[1]] == "X":
+                self.brd[self.selected[0]][self.selected[1]] = 0
+                self.brd[self.selected[0]+1][self.selected[1]-1] = 0
+                self.brd[self.selected[0]+2][self.selected[1]-2] = "X"
+                self.clear_possible_moves()
+                return
+            if self.brd[self.selected[0]][self.selected[1]] == "O":
+                self.brd[self.selected[0]][self.selected[1]] = 0
+                self.brd[self.selected[0]-1][self.selected[1]+1] = 0
+                self.brd[self.selected[0]-2][self.selected[1]+2] = "O"
+                self.clear_possible_moves()
+                return
+        if self.brd[piece_on[0]][piece_on[1]] == "R":
+            if self.brd[self.selected[0]][self.selected[1]] == "X":
+                self.brd[self.selected[0]][self.selected[1]] = 0
+                self.brd[self.selected[0]+1][self.selected[1]+1] = 0
+                self.brd[self.selected[0]+2][self.selected[1]+2] = "X"
+                self.clear_possible_moves()
+                return
+            if self.brd[self.selected[0]][self.selected[1]] == "O":
+                self.brd[self.selected[0]][self.selected[1]] = 0
+                self.brd[self.selected[0]-1][self.selected[1]-1] = 0
+                self.brd[self.selected[0]-2][self.selected[1]-2] = "O"
+                self.clear_possible_moves()
+                return
     def toggle_mouse_press(self):
         self.mousepress = not self.mousepress
         return self.mousepress
--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from board import Board  # Ensure this points to your refactored Board class
 
 # Initialize Pygame
 pygame.init()
-screen = pygame.display.set_mode((400, 400))
+screen = pygame.display.set_mode((1000, 1000))
 pygame.display.set_caption("Checkers")
 
 # Create Board instance
-board_instance = Board(50, 50, screen)
+board_instance = Board(125, 125, screen)
 clock = pygame.time.Clock()
 running = True
 mouse_button_down = False
@@ -15,11 +15,11 @@ mouse_button_down = False
 
 x,y = 0, 0
 while running:
-
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        
         if event.type == pygame.MOUSEBUTTONDOWN and not mouse_button_down: 
             board_instance.select_piece()
         if event.type == pygame.MOUSEBUTTONUP:
@@ -27,7 +27,7 @@ while running:
 
     screen.fill("red")
     board_instance.draw_board()
-
+    
 
     pygame.display.flip()
     clock.tick(60)
